005afcc61345f526e0107dbc96c3bd4eb00f3113
Bruno Leal <bruno_calhau@hotmail.com>
2020-04-21 04:05:08 -0300

    Aula 7 - Tipos de Variaveis ajustes

---
+++ src/variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+	var b int
+	b = 22
+
+	var c string = "Oi Bruno"
+	var d, e string = "Ana", "Rodrigo"
+
+func main() {
+	//Atribuir uma variavel, só posso usar dentro de uma função (Short Hand Method)
+	a := 10
+	// Imprimir uma variavel
+	fmt.Printf("%v \n", a)
+}
