6f30b8a30b52b71fd00ad8aa52a251a06f3085be
Bruno Leal <bruno_calhau@hotmail.com>
2020-04-21 04:11:14 -0300

    Aula 8 - Demonstrando os tipos das variaveis

--- src/variables/main.go
+++ src/variables/main.go
@@ -10,10 +10,10 @@ func main() {
 	d := false
 	e := 'W'
 	f := `Uouuuu`
-	fmt.Printf("%v \n", a)
-	fmt.Printf("%v \n", b)
-	fmt.Printf("%v \n", c)
-	fmt.Printf("%v \n", d)
-	fmt.Printf("%v \n", e)
-	fmt.Printf("%v \n", f)
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%T \n", e)
+	fmt.Printf("%T \n", f)
 }
