00572d8f3cb7c42281a96a5c8e4c24574845c82a
Bruno Leal <bruno_calhau@hotmail.com>
2020-04-22 00:38:05 -0300

    Curso GoLangWeb - Aula 5 - Trabalhando com BD, insert MYSQL

--- src/servidor_web/main.go
+++ src/servidor_web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
@@ -14,11 +17,25 @@ type Post struct {
 
 var post = Post{Id: 1, Title: "Meu primeiro Post", Body: "Esse é meu Body"}
 
+// Criando conexao com DB
+
+var db, err = sql.Open("mysql", "root:root@/go_course?charset=utf8")
+
 func main() {
+
+	//Insert no Banco de dados
+	stmt, err := db.Prepare("Insert into post(title,body) values (?,?)")
+	checkErr(err)
+
+	_, err = stmt.Exec("Meu segundo posttt", "Meu segundo conteudo")
+	checkErr(err)
+
+	// Exemplo de rota padrao
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Hello World")
 	})
 
+	// Exemplo de rota com template
 	http.HandleFunc("/template", func(rw http.ResponseWriter, r *http.Request) {
 		if title := r.FormValue("title"); title != "" {
 			post.Title = title
@@ -31,6 +48,13 @@ func main() {
 		}
 	})
 
+	//Criando servidor de App na porta 8080
 	fmt.Println(http.ListenAndServe(":8080", nil))
 
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
