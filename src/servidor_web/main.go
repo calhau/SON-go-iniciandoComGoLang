fadf98963152092d096aaa6559d9e001ba31a502
Bruno Leal <bruno_calhau@hotmail.com>
2020-04-21 23:08:00 -0300

    Curso GoLangWeb - Aula 4 - Passando valores para Templates, POST/GET - struct

--- src/servidor_web/main.go
+++ src/servidor_web/main.go
@@ -12,15 +12,21 @@ type Post struct {
 	Body  string
 }
 
+var post = Post{Id: 1, Title: "Meu primeiro Post", Body: "Esse é meu Body"}
+
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Hello World")
 	})
 
 	http.HandleFunc("/template", func(rw http.ResponseWriter, r *http.Request) {
+		if title := r.FormValue("title"); title != "" {
+			post.Title = title
+		}
+
 		t := template.Must(template.ParseFiles("templates/index.html"))
 		//t.ExecuteTemplate(rw, "index.html", nil)
-		if err := t.ExecuteTemplate(rw, "index.html", nil); err != nil {
+		if err := t.ExecuteTemplate(rw, "index.html", post); err != nil {
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 		}
 	})
